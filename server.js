df31467e27b5adea02ce210fa1b9cc26101751fe
root <root@DESKTOP-93J1LF5.localdomain>
2025-09-02 18:50:54 +0300

    exercise 2 (/roll/:number)

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 //exercise 1
 app.get('/greetings/:username', (req, res) => {
     const username = req.params.username;
-    res.send(`hello, ${username}!`);
+    res.send(`hi, ${username}!`);
 });
 
 
@@ -13,3 +13,14 @@ app.listen(port, () => {
     console.log(`Listening on port 3000`)
  });
 
+//exercise 2
+app.get('/roll/:number', (req, res) => {
+  const raw = req.params.number;   
+  const n = Number(raw);
+
+  if (!Number.isFinite(n) || !Number.isInteger(n) || n < 0) {
+    return res.status(400).send('You must specify a number.');
+  }
+   const rolled = Math.floor(Math.random() * (n + 1));
+  res.send(`You rolled a ${rolled}.`);
+});
